b76822512a6ed27d7e8af4f38e09ba39efceb87c
TmsRC <t.rubio28@uniandes.edu.co>
2019-04-30 12:59:17 +0000

    Arreglado!

--- graficar.py
+++ graficar.py
@@ -6,7 +6,7 @@ datos = np.loadtxt("datos.dat").T
 
 fig = plt.figure()
 ax = Axes3D(fig)
-ax.plot_wireframe(datos[0],datos[1],datos[2])
+ax.plot_trisurf(datos[0],datos[1],datos[2])
 ax.set_xlabel("X")
 ax.set_ylabel("Y")
 ax.set_zlabel("Potencial")
